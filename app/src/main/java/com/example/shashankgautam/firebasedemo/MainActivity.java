ff0250ec4eb8fb5ba54ea4b9b4ddfd6c6c1b2d09
shashank gautam <shashankankur12@gmail.com>
2018-10-27 11:35:07 +0530

    first commit

--- app/src/main/java/com/example/shashankgautam/firebasedemo/MainActivity.java
+++ app/src/main/java/com/example/shashankgautam/firebasedemo/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
                 String message = dataSnapshot.getValue(String.class);
                 textView.setText(message);
 
+
             }
 
 
