c6e65fdcb71b175a0110f0e2bf953874ec85ec0a
shashank gautam <shashankankur12@gmail.com>
2018-10-27 10:50:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shashankgautam/firebasedemo/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.shashankgautam.firebasedemo;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference mRootRefrence = firebaseDatabase.getReference();
+    private DatabaseReference mChlildRrfrence = mRootRefrence.child("Device");
+    //  private DatabaseReference mChlildRrfrence1=mRootRefrence.child("name");
+    private TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.text_id);
+        mChlildRrfrence.setValue("hello" );
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mChlildRrfrence.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String message = dataSnapshot.getValue(String.class);
+                textView.setText(message);
+
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
+
+
+
+
+
